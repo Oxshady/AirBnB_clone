ce7f7008e2e0a53e1cb5ce9e07f0abd42ec6f1b0
homos <homos@github.com>
2024-05-19 21:58:38 +0300

    test_filestorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import unittest
 from unittest.mock import patch, mock_open
 from models.base_model import BaseModel
@@ -11,6 +10,7 @@ class TestFileStorage(unittest.TestCase):
         self.storage = FileStorage()
         self.base_model = BaseModel()
         self.base_model.id = "1234"
+        FileStorage._FileStorage__objects = {}
         self.storage.new(self.base_model)
 
     def tearDown(self):
@@ -20,6 +20,7 @@ class TestFileStorage(unittest.TestCase):
     def test_all_method(self):
         """Test the all method"""
         all_objects = self.storage.all()
+        self.assertEqual(len(all_objects), 1)
         self.assertIn("BaseModel.1234", all_objects)
 
     def test_new_method(self):
@@ -30,8 +31,17 @@ class TestFileStorage(unittest.TestCase):
         all_objects = self.storage.all()
         self.assertIn("BaseModel.5678", all_objects)
 
+    @patch("builtins.open", new_callable=mock_open)
+    def test_save_method(self, mock_file):
+        """Test the save method"""
+        self.storage.save()
+        mock_file.assert_called_once_with("file.json", "w")
 
-    @patch("builtins.open", new_callable=mock_open, read_data='{"BaseModel.1234": {"__class__": "BaseModel", "id": "1234", "created_at": "2023-05-19T15:22:34.123456", "updated_at": "2023-05-19T15:22:34.123456"}}')
+    @patch(
+        "builtins.open",
+        new_callable=mock_open,
+        read_data='{"BaseModel.1234": {"__class__": "BaseModel", "id": "1234", "created_at": "2023-05-19T15:22:34.123456", "updated_at": "2023-05-19T15:22:34.123456"}}',
+    )
     @patch("os.path.exists", return_value=True)
     def test_reload_method(self, mock_exists, mock_file):
         """Test the reload method"""
@@ -41,6 +51,5 @@ class TestFileStorage(unittest.TestCase):
         self.assertEqual(all_objects["BaseModel.1234"].id, "1234")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
