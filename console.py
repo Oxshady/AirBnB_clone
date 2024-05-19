6eebf98a1f1ab288ca8df3bf202ec9d11bee611f
homos <homos@github.com>
2024-05-19 03:18:16 +0300

    done

--- console.py
+++ console.py
@@ -46,6 +46,7 @@ class HBNBCommand(cmd_hbnb.Cmd):
         the method simply returns `True`
         :return: True
         """
+        print("(hbnb)")
         return True
 
     def help_EOF(self):
