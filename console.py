72d8dedba81d519fb48e24a7c3b167602419d1a1
homos <homos@github.com>
2024-05-19 17:26:08 +0300

    upd

--- console.py
+++ console.py
@@ -83,7 +83,7 @@ class HBNBCommand(cmd_hbnb.Cmd):
         The function "emptyline" does not contain
         any code and simply passes.
         """
-        pass
+        return
 
     def do_create(self, nedded_class):
         """
